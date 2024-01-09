7aeb407e2195027277c6c4137b792199015255f4
Tigit Labs <61022457+tigitlabs@users.noreply.github.com>
2024-01-09 14:28:32 +0000

    test: added default parameter check

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -1,5 +1,24 @@
-def test_bake_project(cookies):
-    result = cookies.bake(extra_context={"repo_name": "helloworld"})
+# Using this plugin:
+# https://github.com/hackebrot/pytest-cookies
+
+
+def test_bake_project_default(cookies):
+    result = cookies.bake()
     assert result.exit_code == 0
-    # Replace result.project with result.project_path
+    assert result.project_path.is_dir()
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    assert result.project_path.name == "typer-cli-template"
+    assert result.project_path.is_dir()
+
+
+def test_bake_project(cookies):
+    result = cookies.bake(extra_context={"project_name": "helloworld"})
+    assert result.exit_code == 0
+    assert result.project_path.is_dir()
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    assert result.project_path.name == "helloworld"
     assert result.project_path.is_dir()
