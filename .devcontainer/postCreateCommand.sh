6d2bd2642c3bbe9a2f2b0bf7f8926c0fc2f57dc3
Timur Yigit <timur_yigit@outlook.com>
2024-01-09 11:40:38 +0000

    tool: devcontainer clean-up

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # .devcontainer/postCreateCommand.sh
-echo "🏗️ Post start command"
+echo "🏗️ Post create command"
 # Check if the script is running on GitHub Codespaces
 if [[ -n "${CODESPACES}" || -n "${GITHUB_CODESPACE_TOKEN}" ]]; then
     printf "Running in GitHub Codespaces.\nNo need to run any commands."
--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # .devcontainer/postStartCommand.sh
-echo "🏗️ Post create command"
+echo "🏗️ Post start command"
 # Check if the script is running on GitHub Codespaces
 if [[ -n "${CODESPACES}" || -n "${GITHUB_CODESPACE_TOKEN}" ]]; then
     printf "Running in GitHub Codespaces.\nNo need to run any commands."
