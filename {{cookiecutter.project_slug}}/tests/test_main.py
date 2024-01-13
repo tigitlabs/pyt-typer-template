8c30a620f9b221e50b8e2781f71806bf53de04bc
Timur <timur@tigitlabs.com>
2024-01-13 23:46:19 +0000

    test: added cli tests

--- {{cookiecutter.project_slug}}/tests/test_main.py
+++ {{cookiecutter.project_slug}}/tests/test_main.py
@@ -1,14 +1,51 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 
+from pathlib import Path
 
+import pytest
+from typer.testing import CliRunner
 from {{cookiecutter.app_slug}} import logger_util, main
 
+runner = CliRunner()
 
-def test_main(capsys):
+
+@pytest.fixture
+def tmp_dir(tmp_path: Path):
+    d = tmp_path
+    print(f"tmp_dir: {d}")
+    assert d.is_dir()
+    return d
+
+
+def test_main(capsys: pytest.CaptureFixture[str]):
     main.hello("Test")
     captured = capsys.readouterr()
     assert "Hello Test" in captured.out
 
 
-def test_logger_init():
-    logger_util.setup_logging("DEBUG")
+def test_logger_init(tmp_dir):
+    print(f"tmp_dir: {tmp_dir}")
+    assert tmp_dir.is_dir()
+    logger_util.setup_logging("DEBUG", log_dest=tmp_dir)
+    assert (tmp_dir / "debug.log").exists()
+
+
+# CLI Tests
+
+
+def test_cli_help():
+    result = runner.invoke(main.app, ["--help"])
+    assert result.exit_code == 0
+    assert "Typer CLI Template Project" in result.stdout
+
+
+def test_cli_init():
+    result = runner.invoke(main.app, ["init"])
+    assert result.exit_code == 0
+    assert "Processed 100 things." in result.stdout
+
+
+def test_cli_hello():
+    result = runner.invoke(main.app, ["hello", "Test"])
+    assert result.exit_code == 0
+    assert "Hello Test" in result.stdout
--- {{cookiecutter.project_slug}}/{{cookiecutter.app_slug}}/logger_util.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_slug}}/logger_util.py
@@ -1,5 +1,7 @@
 # logger_util.py
 import sys
+from pathlib import Path
+from typing import Optional
 
 from loguru import logger
 
@@ -8,10 +10,17 @@ LOG_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS zz}</green> | <level>{level: 
 
 
 # Configure Loguru
-def setup_logging(log_level: str = LOG_LEVEL):
-    # Remove default logger to stdout
+def setup_logging(log_level:str = LOG_LEVEL, log_dest:Optional[Path] = None):
+    if log_dest is None:
+        log_dest = Path("logs")
+    else:
+        log_dest = Path(log_dest)
+        log_dest.mkdir(parents=True, exist_ok=True)
+        assert log_dest.is_dir()
     logger.remove()
+    log_file :Path = log_dest / "debug.log"
     # Add logger for stdout for certain levels (e.g., DEBUG and INFO)
-    logger.add("debug.log", colorize=False, format=LOG_FORMAT, level="DEBUG")
+    logger.add(log_file, colorize=False, format=LOG_FORMAT, level="DEBUG")
     logger.add(sys.stdout, colorize=True, format=LOG_FORMAT, level=log_level)
     logger.debug("Initialized Logger")
+    logger.debug(f"Log destination: {log_dest.absolute()}")
