06908e4008f89f5c4bdedd57e09c912fd79fa0e0
Timur <timur@tigitlabs.com>
2024-01-13 10:31:05 +0000

    tool: devcontainer build improved for local build

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 # .devcontainer/postStartCommand.sh
+ENV_FILE=".devcontainer/.env"
 echo "🏗️ Post start command"
-# Check if the script is running on GitHub Codespaces
-# check if the file 
-
-# TODO this has to be done conditionally check if auth works otherwise configure it
-# if [[ -n "${DEV_CONTAINER_NAME}" || -n "${GITHUB_CODESPACE_TOKEN}" ]]; then
-#     printf "Running in GitHub Codespaces.\nNo need to run any commands."
-# else
-#     echo "Running on local host"
-#     set -o allexport
-#     source .devcontainer/.env
-#     set +o allexport
-#     echo "🧪 Login status for github cli"
-#     gh auth status
-#     echo "🧪 Login docker to ghcr.io for: ${GITHUB_USER}"
-#     echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin
-#     echo "🧪 Check github access via SSH"
-#     # Add github.com to known_hosts
-#     ssh-keyscan github.com >> ~/.ssh/known_hosts
-#     ssh -T git@github.com
-# fi
+echo "Check if running on non GitHub Codespaces"
+# Check if the file exists
+if [ -f "$ENV_FILE" ]; then
+    echo "Running on local host"
+    set -o allexport
+    source .devcontainer/.env
+    set +o allexport
+    echo "🧪 Login status for github cli"
+    gh auth status
+    echo "🧪 Login docker to ghcr.io for: ${GITHUB_USER}"
+    echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USER --password-stdin
+    echo "🧪 Check github access via SSH"
+    # Add github.com to known_hosts
+    mkdir -p ~/.ssh
+    touch ~/.ssh/known_hosts
+    ssh-keyscan github.com >> ~/.ssh/known_hosts
+    # ssh -T git@github.com
+else
+    printf "🐱Running in GitHub Codespaces.\nNo need to run any commands."
+fi
